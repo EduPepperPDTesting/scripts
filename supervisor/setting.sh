b3a3439dfd0344bf4f4bc348dc16931108cce30f
victorfence <victor@education2000.com>
2016-01-07 14:02:31 +0800

    Add app settings for ora.

--- supervisor/setting.sh
+++ supervisor/setting.sh
@@ -1,3 +1,9 @@
+PROJECT_HOME=/home/tahoe/edx_all
+ENV_PYTHON=edx-platform
+
+IMPORT_VIRTUALENVWRAPPER=/usr/bin/virtualenvwrapper.sh
+IMPORT_RVM=/home/tahoe/.rvm/scripts/rvm
+
 PORT_LMS=8000
 PORT_CMS=8001
 PORT_LMS_PREVIEW=8002
@@ -5,12 +11,20 @@ PORT_XQUEUE=8003
 PORT_ORA=8004
 PORT_DISCUSS=8005
 
-PROJECT_HOME=/home/tahoe/edx_all
-ENV_PYTHON=edx-platform
-
 RVM_EDX_PLATFORM="ruby-1.9.3-p448"
 RVM_DISCUSS="ruby-1.9.3-p448@cs_comments_service"
 
-IMPORT_VIRTUALENVWRAPPER=/usr/bin/virtualenvwrapper.sh
-IMPORT_RVM=/home/tahoe/.rvm/scripts/rvm
+export XQUEUE_DB_NAME="xqueue"
+export XQUEUE_DB_USER="pepper"
+export XQUEUE_DB_PASSWORD="lebbeb"
+export XQUEUE_DB_HOST="mysql"
+export XQUEUE_DB_PORT="3306"
 
+export ESSAY_DB_NAME="xqueue"
+export ESSAY_DB_USER="pepper"
+export ESSAY_DB_PASSWORD="lebbeb"
+export ESSAY_DB_HOST="mysql"
+export ESSAY_DB_PORT="3306"
+
+export XQUEUE_URL="http://127.0.0.1:$PORT_XQUEUE"
+export GRADING_CONTROLLER_INTERFACE_URL="http://127.0.0.1:$PORT_ORA"
