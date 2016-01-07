8d51d78953913ec73ab6084ed8b1c2a9cef3e05d
victorfence <victor@education2000.com>
2016-01-07 16:12:46 +0800

    Add more settings.

--- supervisor/setting.sh
+++ supervisor/setting.sh
@@ -14,6 +14,7 @@ PORT_DISCUSS=8005
 RVM_EDX_PLATFORM="ruby-1.9.3-p448"
 RVM_DISCUSS="ruby-1.9.3-p448@cs_comments_service"
 
+# ora
 export XQUEUE_DB_NAME="xqueue"
 export XQUEUE_DB_USER="pepper"
 export XQUEUE_DB_PASSWORD="lebbeb"
@@ -28,3 +29,37 @@ export ESSAY_DB_PORT="3306"
 
 export XQUEUE_URL="http://127.0.0.1:$PORT_XQUEUE"
 export GRADING_CONTROLLER_INTERFACE_URL="http://127.0.0.1:$PORT_ORA"
+
+# discuss
+export ES_INDEX_COMMENT_THREAD="comment_thread_dev"
+export ES_INDEX_COMMENT="comment_thread_dev"
+export DISSCUSS_MONGODB_HOST="mongo"
+export DISSCUSS_MONGODB_PORT="27017"
+
+export COMMENTS_SERVICE_URL="http://localhost:$PORT_DISCUSS"
+
+# edx-platform
+export EDX_PLATFORM_MONGO_HOST='mongo'
+export EDX_PLATFORM_MONGO_PORT=27017
+export EDX_PLATFORM_MONGO_USER='pepper'
+export EDX_PLATFORM_MONGO_PASSWORD='lebbeb'
+export EDX_PLATFORM_MONGO_DB_XMODULE="xmodule"
+export EDX_PLATFORM_MONGO_DB_XCONTENT="xcontent"
+export EDX_PLATFORM_MONGO_DB_USERSTORE="userstore"
+export EDX_PLATFORM_MONGO_DB_REMIND="remind"
+export EDX_PLATFORM_MONGO_DB_ASSIST="assist"
+
+export EDX_PLATFORM_MYSQL_DB_R='pepper'
+export EDX_PLATFORM_MYSQL_HOST_R='mysql_read'
+export EDX_PLATFORM_MYSQL_PORT_R='3306'
+export EDX_PLATFORM_MYSQL_USER_R='pepper'
+export EDX_PLATFORM_MYSQL_PASSWORD_R='lebbeb'
+
+export EDX_PLATFORM_MYSQL_DB_W="$EDX_PLATFORM_MYSQL_DB_R"
+export EDX_PLATFORM_MYSQL_HOST_W='mysql_write'
+export EDX_PLATFORM_MYSQL_PORT_W="$EDX_PLATFORM_MYSQL_PORT_R"
+export EDX_PLATFORM_MYSQL_USER_W="$EDX_PLATFORM_MYSQL_USER_R"
+export EDX_PLATFORM_MYSQL_PASSWORD_W="$EDX_PLATFORM_MYSQL_PASSWORD_R"
+
+export LMS_BASE="http://lms.loc"
+export PREVIEW_LMS_BASE="preview-staging.pepperpd.com"
