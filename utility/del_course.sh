978e6ac25cf79610acba52311258ab8a1c6102a2
victorfence <victor@education2000.com>
2017-03-01 08:16:39 +0800

    Modify command of removing a course.

--- utility/del_course.sh
+++ utility/del_course.sh
@@ -3,6 +3,9 @@
 mongo<<EOF
   use xmodule
   db.modulestore.remove({"_id.org":"$1","_id.course":"$2"})
+  use xcontent
+  db.fs.files.remove({"_id.org":"$1","_id.course":"$2"})
+  db.fs.files.remove({"files_id.org": "$1", "files_id.course": "$2"})
 EOF
 
 
